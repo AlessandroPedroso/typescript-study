677b85eade4c9f0f3e3081ccb6b267d5dd558365
Your Name <alessanrospedroso@outlook.com>
2024-09-06 14:32:24 -0300

    finalizando modulo de entendimento em typeScript

--- src/Utility-types.ts
+++ src/Utility-types.ts
@@ -2,12 +2,32 @@ type User = {
     id: number;
     username: string;
     password: string;
+    createdAt: Date;
+    updatedAt: Date;
 };
 
 const user: User = {
     id: 1,
     username: 'ale123',
-    password: 'typescript123'
+    password: 's44s544',
+    createdAt: new Date(),
+    updatedAt: new Date()
 };
+
+type PartialUser = Partial<User>;
+
+const user2: PartialUser = {
+    id: 1
+}
+
+// OMIT
+type NewUser = Omit<User, 'id'|'username'>
+
+type SearchUser = Pick<User, 'username'>;
+
+const searchUser: SearchUser = {
+    username:'alessandro'
+}
+
 
 
